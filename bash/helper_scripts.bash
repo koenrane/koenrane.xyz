3c3e2b29050a9de66c5fc0f77f655ac9846b3536
koenrane <koenrane@protonmail.com>
2025-02-28 21:09:06 -0600

    bash folder w/ helper scripts

---
+++ bash/helper_scripts.bash
@@ -0,0 +1,9 @@
+#transfer an image via scp from local to remote server
+scp /path/to/local/photo.jpg username@your_droplet_ip:/path/to/remote/folder/
+
+# scp C:/Users/natha/Desktop/repos/koenrane.xyz/pub/images/computer-desk-setup.jpg root@67.205.133.182:/var/www/html/pmwiki/pub/images
+
+# find a directory in linux
+find / -type d -name "myfolder" 2>/dev/null
+
+
