c47183615c8a78b07f8ec8d04613ce27db10cb46
koenrane <koenrane@protonmail.com>
2025-05-23 14:29:52 -0500

    removed breadcrumbs

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,10 +17,10 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.ConditionalRender({
+    /*Component.ConditionalRender({
       component: Component.Breadcrumbs(),
       condition: (page) => page.fileData.slug !== "index",
-    }),
+    }),*/
     Component.ArticleTitle(),
     // Component.ContentMeta(),
     Component.TagList(),
@@ -53,7 +53,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.ArticleTitle()], //Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
